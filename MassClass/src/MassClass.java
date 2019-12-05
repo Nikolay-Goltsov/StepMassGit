ba2b9a3738553bf7fd8b011958e1a43745c1949a
HakerX <HakerX@Haker>
2019-12-06 01:16:24 +0700

    //Проверка в несения изменений

--- MassClass/src/MassClass.java
+++ MassClass/src/MassClass.java
@@ -13,7 +13,7 @@ public class MassClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hi!");
+		System.out.println("Hi!");//Проверка в несения изменений
 
 	}
 
